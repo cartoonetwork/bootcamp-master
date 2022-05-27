a06426395c847551584bb755605009e77b775bb5
tejaswiniyadav1996 <yadavtejaswini939@gmail.com>
2022-05-27 23:19:21 +0530

    array 10 random 3 digit

---
+++ array.sh
@@ -0,0 +1,25 @@
+# Create array
+
+num=(111 222 333 444 555 666 777 888 999 000)
+
+# Print all elements
+
+echo ${num[@]}
+
+# Print 2nd largest element
+
+echo ${num[2]}
+
+# print 2nd smallest element 
+
+echo ${num[1]}
+
+
+
+
+
+# Print length
+
+echo ${#num[@]}
+
+
